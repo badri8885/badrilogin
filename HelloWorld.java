b72ded7721cf639b65261a286d48ba55dd6cb6f2
badri8885 <badri8885@gmail.com>
2023-07-17 20:10:47 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+//your first program
+
+class HelloWorld {
+   public static void main(String[] args){
+   System.out.println("hello, world");
+   System.out.println("hi this is badri");
+   System.out.println("we are learning devops ");
+   
+   }
+   }
+   
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # badrilogin
+# badrilogin
